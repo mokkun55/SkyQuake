5453c6b603dd0bb1410fe5440fa6bcad36e29414
mokkun55 <mokkunpc@gmail.com>
2024-03-12 03:32:12 +0900

    つかれた

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-console.log("サーバーが起動しました");
+//色々読み込み
+const express = require('express');
+const path = require('path');
+const app = express();
+const request = require('request')
+const dotenv = require('dotenv').config();
+
+
+//PORTを指定する
+const PORT = 3000;
+//expressを使い、ローカルサーバーを立ち上げる
+app.listen(PORT, () => console.log(`server is online at http://localhost:${PORT}`));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// / でアクセスのときindex.htmlを返す
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// /weatherでアクセスが来たとき天気情報を返す
+app.get('/weather', (req, res) => {
+    res.send({ weather: weather, chanceOfRains: chanceOfRains, img_url: img_url });
+});
+
+// --お天気API--
+const options = {
+    // 27000はシティID
+    url: `https://weather.tsukumijima.net/api/forecast?city=270000`,
+    method: "GET",
+    json: true,
+};
+
+request(options, (err, res, body) => {
+    // console.log(body.forecasts[1].chanceOfRain)
+
+    day = 1;
+
+    // 天気
+
+    weather = []
+    for (let i = 0; i < 3; i++) {
+        weather.push(body.forecasts[i].telop)
+    }
+
+    // 降水確率
+    chanceOfRains = [] 
+    for (let i = 0; i < 3; i++) {
+        let dailyChanceOfRain = [];
+        dailyChanceOfRain.push(
+            body.forecasts[i].chanceOfRain.T00_06,
+            body.forecasts[i].chanceOfRain.T06_12,
+            body.forecasts[i].chanceOfRain.T12_18,
+            body.forecasts[i].chanceOfRain.T18_24
+        );
+        chanceOfRains.push(dailyChanceOfRain);
+    }
+
+    // console.log(chanceOfRains)
+
+    img_url = []
+    for (let i = 0; i < 3; i++) {
+        img_url.push(body.forecasts[i].image.url)
+    }
+
+})
+
+
+
---
+++ public/script.js
@@ -0,0 +1,82 @@
+// 天気データを受け取る
+
+let weather;
+
+fetch('/weather')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.weather);
+    console.log(data.chanceOfRains);
+    console.log(data.img_url);
+    weather = data.weather;
+
+    const today_weather = document.getElementById("today");
+    today_weather.innerHTML = `
+    <p>${data.weather[0]}</p>
+    <p>${data.chanceOfRains[0]}</p>
+    <img src="${data.img_url[0]}" alt="天気画像">
+    `;
+
+    const tomorrow_weather = document.getElementById("tomorrow");
+    tomorrow_weather.innerHTML = `
+    <p>${data.weather[1]}</p>
+    <p>${data.chanceOfRains[1]}</p>
+    <img src="${data.img_url[1]}" alt="天気画像">
+    `;
+
+    const afttertomorrow_weather = document.getElementById("aftter-tomorrow");
+    afttertomorrow_weather.innerHTML = `
+    <p>${data.weather[2]}</p>
+    <p>${data.chanceOfRains[2]}</p>
+    <img src="${data.img_url[2]}" alt="天気画像">
+    `;
+
+    const rain_radar_city = document.getElementById("rain_radar_city");
+    rain_radar_city.innerHTML = `
+    <img src="${rain_radar_city_URL}" alt="レーダー画像">
+    `;
+
+  })
+  .catch(error => console.error('データの取得中にエラーが発生しました:', error));
+
+pref = "30"; //県ID
+
+// str で渡す
+let year;
+let month ;
+let day ;
+let hour ;
+let min ;
+
+function nowtime(){
+    let date = new Date();
+    year = date.getFullYear(); 
+    month = add_zero(date.getMonth());
+    day = add_zero(date.getDate());
+    hour = add_zero(date.getHours());
+    min = add_zero(Math.floor(date.getMinutes() / 10));
+
+    console.log(year, month, day, hour, min)
+}
+
+function add_zero(num){ // ゼロ補完
+    numStr = num.toString();
+    if(numStr.length === 1){
+      return `0${numStr}`;
+    }else if(numStr.length === 2){
+      return numStr;
+    }
+  }
+
+
+nowtime();
+// if (parseInt(min) <= 10) {
+//     hour = add_zero(hour - 1);
+//     min = "00";
+// } else {
+//     min = min - 10;
+// }
+
+console.log(year, month, day, hour, min)
+rain_radar_city_URL = `https://static.tenki.jp/static-images/radar/${year}/${month}/${day}/${hour}/${min}/00/pref-${pref}-large.jpg`
+// console.log(rain_radar_city_URL)
